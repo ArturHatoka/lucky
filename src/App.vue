31feda29e8e129c16582a3dd60f94ff88f3010db
Ruslan Khasanshin <ruslanfront@ait-corp.net>
2024-02-26 01:18:45 +0600

    Optimized for desctop to 90 performance

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <v-app>
-    <header>
-      <a href="/" class="logo__wrapper">
-        <img alt="logo" class="logo" src="./assets/logo.webp" width="494" height="244">
-      </a>
-    </header>
-
     <v-main>
       <router-view />
     </v-main>
